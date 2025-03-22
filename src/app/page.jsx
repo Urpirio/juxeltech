477faf966226f1ed7262ceefa7cb8976f1764abc
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-21 23:18:38 -0400

    Se agrego el componente Home

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import BestServices from '@/components/BestServices';
+import Section1 from '@/components/Section1';
+import React from 'react';
+
+export default function page() {
+  return (
+    <main>
+      <Section1/>
+      <BestServices/>
+    </main>
+  )
+}

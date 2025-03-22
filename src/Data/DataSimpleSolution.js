0ad09e6cac364d34d93394fa2aedc5bc149fba77
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-22 08:14:41 -0400

    Se agrego el array  de datos de Data simple solution

---
+++ src/Data/DataSimpleSolution.js
@@ -0,0 +1,23 @@
+const DataSimpleSolution = [
+    {
+        number: '1',
+        title: 'Contact us',
+        description: 'Contact us to boost your brand visibility.',
+    },
+    {
+        number: '2',
+        title: 'Consult',
+        description: 'Always available to answer your questions.',
+    },
+    {
+        number: '3',
+        title: 'Place order',
+        description: 'Buy our package today to proceed.',
+    },
+    {
+        number: '4',
+        title: 'Payment',
+        description: 'We receives payments in all types of banking.',
+    }
+];
+export default DataSimpleSolution;

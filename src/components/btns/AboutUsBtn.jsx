12306f27a4ed91c4f052ef6854cd5413cadfca92
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-22 09:46:29 -0400

    Se agrego un componente Btn

---
+++ src/components/btns/AboutUsBtn.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AboutUsBtn() {
+  return (
+        <button className='border-1 bg-blue-500 text-white font-semibold rounded-2xl p-2 w-30'>Read more</button>
+  )
+}

ffe74c1a7c7d1e71d063c16a3d29479d1d5b6d96
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-21 23:18:26 -0400

    Se agrego el componente layout

---
+++ src/app/layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../Style/Globals.css';
+import Header from '@/components/Header';
+
+export default function layout({children}) {
+  return (
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
+        <title>JuxelTech</title>
+    </head>
+    <body>
+      <Header/>
+        {children}
+    </body>
+    </html>
+  )
+}

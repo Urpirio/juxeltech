eafe8aacc88d3c40849d8f7961c92af82749dd9e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-22 08:12:58 -0400

    Se agrego un nuevo sub componente

---
+++ src/components/SubComponents/SubSimpleSolution.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SubSimpleSolution(props) {
+  return (
+    <article className='flex items-center  justify-left gap-5'>
+        <div className='h-10 w-10 flex justify-center items-center bg-blue-500 rounded-[100%] text-white text-xl'>
+            <span>{props.number}</span>
+        </div>
+                            
+        <div className='flex flex-col justify-baseline w-[70%]'>
+            <h3 className='text-[18px] font-semibold'>{props.title}</h3>
+            <p className='font-normal text-gray-600' >{props.description}</p>
+        </div>
+    </article>
+  )
+}

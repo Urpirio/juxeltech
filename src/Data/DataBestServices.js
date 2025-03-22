c4065013fed5a5c21fd071fa6acc6278428a3f1a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-21 23:20:06 -0400

    Se agrego el DataBest Services para las tarjestas

---
+++ src/Data/DataBestServices.js
@@ -0,0 +1,27 @@
+const DataBestServices = [
+    {
+       Service: 'Seo/Sem',
+       Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quidem. Lorem ipsum dolor sit amet.',
+       Icon: 'bx bx-search-alt', 
+       bg: 'bg-amber-400',
+    },
+    {
+        Service: 'Marketing',
+        Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quidem. Lorem ipsum dolor sit amet.',
+        Icon: 'bx bxs-megaphone', 
+        bg: 'bg-blue-600',
+     },
+     {
+        Service: 'Viral Campaign',
+        Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quidem. Lorem ipsum dolor sit amet.',
+        Icon: 'bx bx-tv',
+        bg: 'bg-violet-600 ',
+     },
+     {
+        Service: 'Others',
+        Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quidem. Lorem ipsum dolor sit amet.',
+        Icon: 'bx bx-receipt',
+        bg: 'bg-orange-600', 
+     },
+];
+export default DataBestServices;

28f13072d4e5f8c1889f149f22dca5798e29486f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-22 22:43:35 -0400

    Se agrego el array de datos

---
+++ src/Data/DataClientsSay.js
@@ -0,0 +1,80 @@
+import ClientsEscobar from '@/../public/ClientsSay/ClientsEscobar.png';
+import ClientsEsteban from '@/../public/ClientsSay/ClientsEsteban.png';
+import ClienstValeria from '@/../public/ClientsSay/ClientsValeria.png';
+
+const DataClientsSay =[
+    {
+        Image: ClientsEscobar,
+        Name: 'Martín Escobar',
+        ProfesionalArea: 'Software Developer',
+        Comment: 'This code editor is amazing… until it crashes right before I save.',
+        Idstar: 'Client1',
+        Star: [
+            {
+                classStar: 'bx bxs-star'
+            },
+            {
+                classStar: 'bx bxs-star'
+            },
+            {
+                classStar: 'bx bxs-star'
+            },
+            {
+                classStar: 'bx bxs-star'
+            },
+            {
+                classStar: 'bx bxs-star-half'
+            },
+        ]
+    },
+    {
+        Image: ClienstValeria,
+        Name: 'Valeria Montenegro',
+        ProfesionalArea: 'Cybersecurity Analyst',
+        Comment: 'Their ‘secure’ system had ‘password123’ as the admin login. Impressive',
+        Idstar: 'Client2',
+        Star: [
+            {
+                classStar: 'bx bxs-star'
+            },
+            {
+                classStar: 'bx bxs-star'
+            },
+            {
+                classStar: 'bx bxs-star'
+            },
+            {
+                classStar: 'bx bxs-star-half'
+            },
+            {
+                classStar: 'bx bx-star'
+            },
+            
+        ]
+    },
+    {
+        Image: ClientsEsteban,
+        Name: 'Esteban Ramírez',
+        ProfesionalArea: 'UX Designer',
+        Comment: 'The app looks sleek, but finding the logout button is like solving a puzzle.',
+        Idstar: 'Client3',
+        Star: [
+            {
+                classStar: 'bx bxs-star'
+            },
+            {
+                classStar: 'bx bxs-star'
+            },
+            {
+                classStar: 'bx bxs-star'
+            },
+            {
+                classStar: 'bx bxs-star-half'
+            },
+            {
+                classStar: 'bx bx-star'
+            },
+        ]
+    },
+];
+export default DataClientsSay;

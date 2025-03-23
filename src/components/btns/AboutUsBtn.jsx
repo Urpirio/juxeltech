a3032d9dc1f3ad8ea040a9d446bdaf2260b2538d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-22 22:42:54 -0400

    se agregaron detalles extras al estilo

--- src/components/btns/AboutUsBtn.jsx
+++ src/components/btns/AboutUsBtn.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export default function AboutUsBtn() {
   return (
-        <button className='border-1 bg-blue-500 text-white font-semibold rounded-2xl p-2 w-30'>Read more</button>
+        <button className='border-1 bg-blue-500 text-white font-semibold rounded-2xl p-2 w-30 transition-all duration-200 cursor-pointer hover:opacity-75'>Read more</button>
   )
 }

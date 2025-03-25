84a824884364d63faa5d0eabbe5a401871c31294
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-25 19:16:15 -0400

    Data Array de About section

---
+++ src/app/AboutUs/Data/DataAboutSections.js
@@ -0,0 +1,24 @@
+import BookHistory from '@/../public/AboutUs/BookHistory.png';
+import RocketAbourUs from '@/../public/AboutUs/RocketAboutUs.png';
+
+const DataAboutSection = [
+    {
+        Image: RocketAbourUs,
+        title: 'Mission',
+        Text1: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
+        Text2: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
+        classOrder: '',
+        classAlign: '',
+        padding: 'p-8',
+    },
+    {
+        Image: BookHistory,
+        title: 'Story',
+        Text1: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
+        Text2: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
+        classOrder: 'flex-row-reverse',
+        classAlign: 'items-center',
+        padding: 'p-5',
+    },
+];
+export default DataAboutSection;

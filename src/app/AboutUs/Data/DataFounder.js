e9ea1eeaa33f1419866640ea34102ad0f7b017d7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-25 22:45:50 -0400

    Se agrego un nuevo array de datos

---
+++ src/app/AboutUs/Data/DataFounder.js
@@ -0,0 +1,23 @@
+import ImgPrueba from '@/../public/AboutUs/AboutUsPage/ImgPrueba.png'
+
+const DataFounter = [
+    {
+        Img: ImgPrueba,
+        name: 'Urpirio Junior Moreno',
+        work: 'Software Developer', 
+        About: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
+    },
+    {
+        Img: ImgPrueba,
+        name: 'Nelson Javier',
+        work: 'Software Developer', 
+        About: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
+    },
+    {
+        Img: ImgPrueba,
+        name: 'Enmanuel Batuze',
+        work: 'Software Developer', 
+        About: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
+    }
+];
+export default DataFounter;

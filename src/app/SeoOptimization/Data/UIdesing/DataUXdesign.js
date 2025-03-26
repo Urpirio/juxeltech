0ca189df99ff2427920beea3a29d8b9ca950000d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 15:49:56 -0400

    Se agrego el Array de datos para DataUXdesign

---
+++ src/app/SeoOptimization/Data/UIdesing/DataUXdesign.js
@@ -0,0 +1,29 @@
+const DataUXdesign = [/*UX Design (User Experience Design)*/
+    {
+        number: `1`,
+        title: `User Research:`,
+        description: `Understanding user needs, behaviors, and challenges through research methods such as surveys, interviews, and user testing.`,
+    },
+    {
+        number: `2`,
+        title: `User Flows & Wireframing: `,
+        description: `Designing the path a user will take through the app or website, creating wireframes and prototypes to test the layout and functionality.`,
+    },
+    {
+        number: `3`,
+        title: `Usability Testing: `,
+        description: `Testing the design with real users to identify pain points and areas for improvement.`,
+    },
+    {
+        number: `4`,
+        title: `Information Architecture: `,
+        description: ` Organizing content and features in a way that’s logical and easy to find, reducing friction for users.`,
+    },
+    {
+        number: `5`,
+        title: `Responsive Design:`,
+        description: `Ensuring the design works well across different devices (mobile, tablet, desktop), providing a seamless experience everywhere.`,
+    },
+
+];
+export default DataUXdesign;

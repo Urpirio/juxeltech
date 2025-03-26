da1af1e423407d14e26f98d93de7ffb54a0bb1c2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 15:42:07 -0400

    Se agregaron mejoras a la responsividad

--- src/app/AboutUs/components/Founder.jsx
+++ src/app/AboutUs/components/Founder.jsx
@@ -17,12 +17,12 @@ export default function Founder() {
     })
 
   return (
-    <section className='flex flex-col items-center'>
+    <section className='flex flex-col items-center p-10'>
         <div className='text-center'>
             <h2 className='text-3xl'>Our <span className='text-blue-600'>Founder</span></h2>
             <p className='text-[18px]'>Our team  consists only of the best talents</p>
         </div>
-        <div className='flex flex-col gap-5 justify-center items-center w-[100%] min-w-80 p-8'>
+        <div className='flex flex-wrap gap-5 justify-center items-center w-[100%] min-w-80 p-8'>
             {FoundersList}
         </div>
     </section>

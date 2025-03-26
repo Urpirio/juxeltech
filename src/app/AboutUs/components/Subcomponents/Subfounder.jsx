8ded1e1141d267fd98353984c45ab4017273b31e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 15:44:19 -0400

    Se hicieron cambios al estilo

--- src/app/AboutUs/components/Subcomponents/Subfounder.jsx
+++ src/app/AboutUs/components/Subcomponents/Subfounder.jsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 
 export default function Subfounder(props) {
   return (
-    <article className='flex flex-wrap justify-center items-center gap-5 sm:border-0 sm:shadow-none shadow-xl rounded-b-2xl rounded-t-2xl'>
-                <div className='w-80 sm:w-50 sm:h-40.5  rounded-2xl  shadow-2xs'>
-                    <Image className='rounded-t-2xl sm:rounded-2xl sm:border-1 sm:border-gray-200 sm:h-40 w-80' src={props.Img} alt={props.name}/>
+    <article className='flex flex-wrap justify-center items-center w-80  shadow-xl rounded-b-2xl rounded-t-2xl'>
+                <div className='w-80 rounded-2xl  shadow-2xs'>
+                    <Image className='rounded-t-2xl' src={props.Img} alt={props.name}/>
                 </div>
-                <div className='flex flex-col gap-1 w-[50%] min-w-80 p-5 rounded-2xl sm:border-1 sm:border-gray-200 sm:shadow-xl '>
+                <div className='flex flex-col gap-1 w-[50%] min-w-80 p-5 rounded-2xl '>
                     <div className='flex flex-col justify-center'>
                         <h3 className='text-blue-600 text-2xl font-bold'>{props.name}</h3>
                         <span className='text-gray-600 text-[14px] font-semibold'>{props.work}</span>

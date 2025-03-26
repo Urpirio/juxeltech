34068698ee6039370ad68692d502f57a6dd03a3d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 15:47:39 -0400

    Se agrego el Componente UIdesign que habla sobre la ui

---
+++ src/app/SeoOptimization/components/UIDesign.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function UIDesign() {
+  return (
+    <section className='flex flex-col justify-center items-center bg-gray-50 text-left sm:text-center p-5 sm:p-10'>
+        <div className='w-[80%] min-w-80'>
+            <h1 className='text-3xl font-semibold'>What is <span className='text-blue-600'>UI/UX Design?</span></h1>
+        </div>
+        <div className='w-[80%] min-w-80'>
+            <p>
+            Refers to the design of user interfaces (UI) and user experiences (UX) for websites,
+            applications, and other digital products. These two components work together to ensure
+            a product is both visually appealing (UI) and easy to use (UX). Here’s what each part includes:
+            </p>
+        </div>
+    </section>
+  )
+}

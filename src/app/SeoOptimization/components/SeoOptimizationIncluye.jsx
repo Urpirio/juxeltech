7b1183e6048a6aa72f92d091ab5f9447a34250d0
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 15:46:30 -0400

    Se agrego el componente SeoOptimizationIncluye

---
+++ src/app/SeoOptimization/components/SeoOptimizationIncluye.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import DataSeoServices from '../Data/SeoServices/DataSeoServices'
+import SubAboutServices from './Subcomponents/SubAboutServices'
+import DataSeoServices2 from '../Data/SeoServices/DataSeoServices2';
+
+export default function SeoOptimizationIncluye() {
+
+    const SeoServiceList1 = DataSeoServices.map(DSS => {
+        return(
+            <SubAboutServices
+            number = {DSS.number}
+            title = {DSS.title}
+            description = {DSS.description}
+            />
+        )
+    });
+
+    const SeoServiceList2 = DataSeoServices2.map(DSS => {
+        return(
+            <SubAboutServices
+            number = {DSS.number}
+            title = {DSS.title}
+            description = {DSS.description}
+            />
+        )
+    })
+
+
+  return (
+    <section className='flex flex-wrap justify-center items-center gap-5 p-10'>
+        <div className='flex flex-col gap-5 w-[40%] min-w-80'>
+            {SeoServiceList1}
+        </div>
+        <div className='flex flex-col gap-5 w-[40%] min-w-80'>
+            {SeoServiceList2}
+        </div>
+
+    </section>
+  )
+}

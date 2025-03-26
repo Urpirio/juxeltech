b46da76f8df44a50b5b93188c8e608bc478354eb
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 15:48:30 -0400

    Se agrego el array de datos SeoServicess

---
+++ src/app/SeoOptimization/Data/SeoServices/DataSeoServices.js
@@ -0,0 +1,24 @@
+const DataSeoServices = [
+    {
+        number: `1`,
+        title: `Keyword Research:`,
+        description: `Identifying keywords relevant to your business.`,
+    },
+    {
+        number: `2`,
+        title: `On-Page Optimization:`,
+        description: `Internal site improvements like optimizing titles, content, images, and URLs.`,
+    },
+    {
+        number: `3`,
+        title: `Off-Page Optimization:`,
+        description: `External strategies like link building and content marketing.`,
+    },
+    {
+        number: `4`,
+        title: `Mobile Optimization:`,
+        description: `Ensuring your site is fully functional on mobile devices.`,
+    },
+
+];
+export default DataSeoServices;

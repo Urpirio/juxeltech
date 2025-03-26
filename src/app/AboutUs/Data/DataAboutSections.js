1e23da2407d0507332a15255009f87783a3727f2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 15:44:46 -0400

    se agrego el contenido de las historia y mission

--- src/app/AboutUs/Data/DataAboutSections.js
+++ src/app/AboutUs/Data/DataAboutSections.js
@@ -5,8 +5,9 @@ const DataAboutSection = [
     {
         Image: RocketAbourUs,
         title: 'Mission',
-        Text1: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
-        Text2: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
+        Text1: `At JuxelTech, our mission is to transform the future by creating technological solutions that not only improve business processes but also have a real, positive impact on people's lives. We believe in the power of innovation to create applications that not only optimize business performance and efficiency but also catalyze social and personal change.`,
+        Text2: `We are dedicated to developing high-impact digital solutions for businesses, with the goal of boosting their growth, adaptability, and competitiveness. But beyond that, we are committed to creating applications that transform lives, enabling technology to serve as a bridge to a better, more inclusive, and accessible future for all.`,
+        Text3: ``,
         classOrder: '',
         classAlign: '',
         padding: 'p-8',
@@ -14,11 +15,12 @@ const DataAboutSection = [
     {
         Image: BookHistory,
         title: 'Story',
-        Text1: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
-        Text2: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
+        Text1: `JuxelTech began in 2025, when three young individuals with a shared vision decided to come together and create something that would make a difference. United by a passion for technology and the desire to change the job landscape in web and mobile design and development, we set out on a clear mission: to significantly impact the future of both people and businesses, opening new doors for those who need them most.`,
+        Text2: `From day one, our goal has been clear: to build a platform that not only helps businesses find the right talent but also offers individuals the opportunity to gain experience in a sector where opportunities are limited. We aim for profiles to align 100% with the needs of companies, eliminating the traditional barriers of the job market.`,
+        Text3: `But our story isn’t over yet. Each day, we continue to write it, driven by a vision of a more accessible, inclusive, and dynamic future. What started as an idea in our minds is now evolving into a software agency with the potential to change the course of thousands of lives. However, this is just the beginning.`,
         classOrder: 'flex-row-reverse',
         classAlign: 'items-center',
-        padding: 'p-5',
+        padding: 'p-5 ',
     },
 ];
 export default DataAboutSection;

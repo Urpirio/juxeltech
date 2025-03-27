a88dfcb83b22422f6be589c13fc33f7683520435
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 22:16:02 -0400

    se agrego un array de datos para renderizar las caracterizticas de un desarrollo backend

---
+++ src/app/SeoOptimization/Data/WebDevelopment/Backend/DataBackendDevelopment.js
@@ -0,0 +1,19 @@
+const DataBackendDevelopment = [
+     /*Lista de elementos*/
+    {
+        number: `1`,
+        title: `Server-Side Logic: `,
+        description: `Manages data processing, authentication, and integrations.`,
+    },
+    {
+        number: `2`,
+        title: `Database Management: `,
+        description: `Stores and retrieves user information securely.`,
+    },
+    {
+        number: `3`,
+        title: `API Development:`,
+        description: `Connects the frontend with external services and databases.`,
+    }
+];
+export default DataBackendDevelopment;

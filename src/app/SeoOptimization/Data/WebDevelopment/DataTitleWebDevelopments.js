d781c29d53bf90dc9c9860f819e213cbf152f74d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 22:17:20 -0400

    Se agrego el array de datos para le inicio del apartado de webdevelopment

---
+++ src/app/SeoOptimization/Data/WebDevelopment/DataTitleWebDevelopments.js
@@ -0,0 +1,9 @@
+const DataTitleWebDevelopments = [
+    {
+        titlePart1: 'About our',
+        titlePart2: 'web development',
+        description: `Web development involves building, maintaining, and optimizing websites and web
+        applications to ensure functionality, performance, and user experience. It includes:`,
+    }
+];
+export default DataTitleWebDevelopments;

7f995695ce504e1ecac1ebce7a8b2afb9d80054d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 23:43:22 -0400

    Mejoras en la responsividad

--- src/app/SeoOptimization/components/HowUIDesign.jsx
+++ src/app/SeoOptimization/components/HowUIDesign.jsx
@@ -32,15 +32,15 @@ export default function HowUIDesign() {
 
 
   return (
-    <section className='flex flex-col'>
-        <header className='flex justify-start sm:justify-center p-2'>
+    <section className='flex flex-col gap-5 py-5'>
+        <header className='flex justify-start sm:justify-center px-5'>
             <h2 className='text-3xl font-semibold'>How UI/UX Design <span className='text-blue-600'>Benefits Your Business?</span></h2>
         </header>
-        <footer className='flex flex-wrap justify-center items-center p-10'>
-            <div className='flex flex-col gap-5 w-[40%] min-w-90'>
+        <footer className='flex flex-wrap justify-center items-center'>
+            <div className='flex flex-col gap-5 w-[40%] min-w-90 px-5'>
                 {HowUIDesignList}
             </div>
-            <div className='flex flex-col gap-5 w-[40%] min-w-90'>
+            <div className='flex flex-col gap-5 w-[40%] min-w-90 px-5'>
                 {HowUIDesignList2}
             </div>
         </footer>

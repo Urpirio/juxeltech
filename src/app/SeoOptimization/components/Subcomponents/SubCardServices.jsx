afc6df0ece7e751261c978185bfc919627871e85
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 23:43:42 -0400

    Mejoras en la responsividad

--- src/app/SeoOptimization/components/Subcomponents/SubCardServices.jsx
+++ src/app/SeoOptimization/components/Subcomponents/SubCardServices.jsx
@@ -6,7 +6,7 @@ export default function SubCardServices(props) {/*Este elemento en el componente
             <header className='text-2xl text-blue-500 font-semibold'>
                 <h2>{props.Titles}</h2>
             </header>
-            <div className=' flex flex-col gap-2 w-90'>
+            <div className=' flex flex-col gap-2 sm:w-80'>
                 {props.list}
             </div>
    </article>

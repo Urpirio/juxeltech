8e99975f770ef3ddfa330d0d19d3fd5ce7bd0567
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-27 19:54:15 -0400

    Correcion de errores en las responsividad

--- src/app/Services/components/FrequentlyQuestions.jsx
+++ src/app/Services/components/FrequentlyQuestions.jsx
@@ -23,15 +23,17 @@ export default function FrequentlyQuestions() {
 
 
   return (
-    <section className="flex flex-col items-center py-10 gap-y-5 ">
+    <section className="flex flex-col items-center py-10 gap-y-5">
         <div className="flex justify-center">
             <div className="flex flex-col items-center gap-2">
-                <h2 className="text-3xl">Frequently asked <span className="text-blue-600">Question</span></h2>
+                <h2 className="sm:text-3xl text-[28px] ">Frequently asked <span className="text-blue-600">Question</span></h2>
                 <div className="w-20 h-1 bg-blue-600 rounded-2xl"></div>
             </div>
         </div>
-        <div className="flex flex-col justify-center w-[50%] min-w-90 sm:min-w-140 border-1 border-blue-400 rounded-2xl shadow-xl ">
-            {Questionlist}
+        <div className=" flex p-5">
+             <div className="flex flex-col justify-center w-[50%] min-w-80 sm:min-w-140 border-1 border-blue-400 rounded-2xl shadow-xl">
+                {Questionlist}
+            </div>
         </div>
     </section>
   )
--- src/app/Services/components/Subcomponents/SubFrequentlyQuestion.jsx
+++ src/app/Services/components/Subcomponents/SubFrequentlyQuestion.jsx
@@ -7,11 +7,11 @@ export default function SubFrequentlyQuestion(props) {
 
 
   return (
-    <article className='flex flex-col items-center'>
+    <article className='flex flex-col items-center p-1'>
         <div onClick={()=>{DeployQuestion(props.NumberQuestion)}} className='flex items-center justify-between w-[100%] p-2 text-blue-600 border-b-1 border-gray-200 rounded-t-2xl'>
-            <h2 className='text-xl'>{props.question}</h2><i className={`bx bx-chevron-right text-4xl transition-transform duration-300 ${props.ClassIcon}`}></i>
+            <h2 className='text-xl '>{props.question}</h2><i className={`bx bx-chevron-right text-4xl transition-transform duration-300 ${props.ClassIcon}`}></i>
         </div>
-        <div className={`hidden text-gray-700 w-[100%] p-2 border-b-1 border-gray-200 
+        <div className={`hidden text-gray-700 w-[100%] p-1 border-b-1 border-gray-200 
          `} id={props.classQuetion}>
             <p className={`${props.Animaciones}`}>
             {props.answer}

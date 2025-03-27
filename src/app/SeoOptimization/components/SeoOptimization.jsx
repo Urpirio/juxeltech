4b245386cd2d07cec9c5d26b9da75b9d2817102a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 22:10:00 -0400

    Se optimizo la manera de renderizar los elementos

--- src/app/SeoOptimization/components/SeoOptimization.jsx
+++ src/app/SeoOptimization/components/SeoOptimization.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from 'react';
+import DataTitleSeoServices from '../Data/SeoServices/DataTitleSeoServices';
+import SubTitleServices from './Subcomponents/SubTitleServices';
 
 export default function SeoOptimization() {
-  return (
-    <section className='flex flex-col justify-center items-center bg-gray-50 text-left sm:text-center p-5 sm:p-10'>
-        <div className='w-[80%] min-w-80'>
-            <h1 className='text-3xl font-semibold'>What the <span className='text-blue-600'>Seo Optimization?</span></h1>
-        </div>
-        <div className='w-[80%] min-w-80'>
-            <p>
-            It is a set of strategies and techniques implemented on a website to improve its visibility and 
-            positioning in search engines, such as Google. This translates into increased organic traffic to
-            your site, which increases the likelihood of conversion and growth for your business.Here I explain
-            what this section includes:
-            </p>
-        </div>
-    </section>
-  )
-}
+
+    const SeoOptimizationlist = DataTitleSeoServices.map(DTSS => {
+        return(
+            <SubTitleServices
+            titlePart1 = {DTSS.titlePart1}
+            titlePart2 ={DTSS.titlePart2}
+            description = {DTSS.description}
+            />
+        )
+    })
+
+  return (SeoOptimizationlist)};

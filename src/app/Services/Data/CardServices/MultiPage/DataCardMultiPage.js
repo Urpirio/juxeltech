ee67a48deb3a7266ce6fb19128cb434e55ada1c1
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-27 13:52:49 -0400

    Se agrego un icono y el precio

--- src/app/Services/Data/CardServices/MultiPage/DataCardMultiPage.js
+++ src/app/Services/Data/CardServices/MultiPage/DataCardMultiPage.js
@@ -1,10 +1,10 @@
 const DataCardMultiPage = [
     {
         NameService: `Multipage Website`,
-        PriceService: ``,
+        PriceService: `500 - 1000`,
         DescriptionService: `A website with multiple interconnected
         pages, ideal for businesses with more structured content.`,
-        classIcon: `bx bx-buildings`,
+        classIcon: `bx bx-windows`,
         BgHeader: `bg-blue-600`,
         BgFooterBtn: `bg-blue-600`,
         listdescription: [

a4d085c9b68e4e1502807cbda2d223789fd56843
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-27 19:40:30 -0400

    Se agrego el array de datos de preguntas frecuentes

---
+++ src/app/Services/Data/FrequentlyQuestion/DataFrequentlyQuestion.js
@@ -0,0 +1,70 @@
+// 'use client'
+const DataFrenquentlyQuestion = [
+    {
+        NumberQuestion: 1,
+        question: `How long does it take to develop a website?`,
+        answer: `It depends on the type of website. A Onepage site
+        takes about 1 to 3 weeks, while a multipage or dynamic site
+        may take 4 to 8 weeks. E-commerce websites usually take 6 to
+        12 weeks, depending on the required features.`,
+        State: false,
+        classQuetion: `QuestionService1`,
+        ClassIcon: `IconQuestionService1`,
+        Animaciones: `sm:animate-[AnswerAnimation1Start_1s]
+        animate-[AnswerAnimation4Start_1s]`,
+
+    },
+    {
+        NumberQuestion: 2,
+        question: `What is included in the web development service?`,
+        answer: ` It includes custom design, responsive development,
+        SEO optimization, integration with necessary tools (forms, 
+        databases, payment gateways), and testing before delivery.`,
+        State: false,
+        classQuetion: `QuestionService2`,
+        ClassIcon: `IconQuestionService2`,
+        Animaciones: `lg:animate-[AnswerAnimation2Start_1s] 
+        sm:animate-[AnswerAnimation1Start_1s]
+        animate-[AnswerAnimation5Start_1s]`,
+    },
+    {
+        NumberQuestion: 3,
+        question: `Are the websites mobile-friendly?`,
+        answer: `Yes, all our websites are fully responsive and work
+        perfectly on desktops, tablets, and mobile devices.`,
+        State: false,
+        classQuetion: `QuestionService3`,
+        ClassIcon: `IconQuestionService3`,
+        Animaciones: `lg:animate-[AnswerAnimation3Start_1s] 
+        sm:animate-[AnswerAnimation2Start_1s]
+        animate-[AnswerAnimation1Start_1s]`,
+    },
+    {
+        NumberQuestion: 4,
+        question: `Can I manage my website after delivery?`,
+        answer: `Yes, if the website is dynamic or e-commerce, we 
+        provide an admin panel so you can easily manage content,
+        products, and users.`,
+        State: false,
+        classQuetion: `QuestionService4`,
+        ClassIcon: `IconQuestionService4`,
+        Animaciones: `lg:animate-[AnswerAnimation2Start_1s]
+        sm:animate-[AnswerAnimation2Start_1s]
+        animate-[AnswerAnimation1Start_1s]`,
+    },
+    {
+        NumberQuestion: 5,
+        question: `What if I need changes after the website is delivered?`,
+        answer: `We offer support and maintenance services for updates, 
+        improvements, and troubleshooting. We can also add new features
+        based on your needs.`,
+        State: false,
+        classQuetion: `QuestionService5`,
+        ClassIcon: `IconQuestionService5`,
+        Animaciones: `lg:animate-[AnswerAnimation2Start_1s]
+        sm:animate-[AnswerAnimation2Start_1s] 
+        animate-[AnswerAnimation5Start_1s]`,
+    },
+
+];
+export default DataFrenquentlyQuestion;

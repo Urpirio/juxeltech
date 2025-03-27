7f35b54b81c38eb43bc5a8d527960f83f477de99
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 22:18:49 -0400

    Se agrego el array de datos para el mantenimiento

---
+++ src/app/SeoOptimization/Data/WebDevelopment/Sopport/DataSopportDevelopment.js
@@ -0,0 +1,20 @@
+const DataSopportDevelopment = [
+     /*Lista de elementos*/
+     /*Datos del apartado Sopport*/
+    { 
+        number: `1`,
+        title: `Bug Fixes & Updates:`,
+        description: `Keeps the website secure and up to date.`,
+    },
+    {
+        number: `2`,
+        title: `Performance Monitoring:`,
+        description: `Ensures speed and stability.`,
+    },
+    {
+        number: `3`,
+        title: `Feature Enhancements:`,
+        description: `Adapts to business needs and user feedback.`,
+    },
+];
+export default DataSopportDevelopment;

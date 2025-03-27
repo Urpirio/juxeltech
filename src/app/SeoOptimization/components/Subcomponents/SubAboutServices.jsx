c7c6f34f7cd1ce7eea6067232da50365b0a57087
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 23:43:38 -0400

    Mejoras en la responsividad

--- src/app/SeoOptimization/components/Subcomponents/SubAboutServices.jsx
+++ src/app/SeoOptimization/components/Subcomponents/SubAboutServices.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SubAboutServices(props) {/*Este elemento se utiliza en casi todos los componentes*/
   return (
     <article className='flex items-center gap-5'>
-        <div className='h-10 w-10 flex justify-center items-center border-1 rounded-[10px] bg-blue-600 text-white text-[18px] font-semibold '>
+        <div className='h-10 w-10 flex justify-center items-center border-1 rounded-[100px] bg-blue-600 text-white text-[18px] font-semibold '>
             <span>{props.number}</span>
         </div>
         <div className='flex flex-col w-[100%]'>

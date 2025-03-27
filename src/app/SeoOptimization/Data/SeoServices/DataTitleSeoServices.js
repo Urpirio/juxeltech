282751ef76e11b2f32d5b5edd19026c00951f4ef
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 22:14:20 -0400

    Se agrego un array de datos para el  componente seo

---
+++ src/app/SeoOptimization/Data/SeoServices/DataTitleSeoServices.js
@@ -0,0 +1,12 @@
+
+const DataTitleSeoServices = [
+    {
+        titlePart1: 'What the',
+        titlePart2: 'Seo Optimization?',
+        description: `It is a set of strategies and techniques implemented on a website to improve its visibility and 
+        positioning in search engines, such as Google. This translates into increased organic traffic to
+        your site, which increases the likelihood of conversion and growth for your business.Here I explain
+        what this section includes:`,
+    }
+];
+export default DataTitleSeoServices;

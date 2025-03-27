bfb935d75429b710fcc4fc82929b673dd72e1a92
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-26 23:43:56 -0400

    Mejoras en la responsividad
    Se agregaron paddings

--- src/app/SeoOptimization/components/Subcomponents/SubTitleServices.jsx
+++ src/app/SeoOptimization/components/Subcomponents/SubTitleServices.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function SubTitleServices(props) {/*Este elemento en el componente todos los titles antes de hablar de algun tema*/
   return (
-    <section className='flex flex-col justify-center items-center bg-gray-50 text-left sm:text-center p-5 sm:p-10'>
-        <div className='w-[80%] min-w-90'>
+    <section className='flex flex-col justify-center items-center bg-gray-50 text-left sm:text-center sm:p-10'>
+        <div className='w-[80%] min-w-90 pl-5 pr-5'>
             <h1 className='text-3xl font-semibold'>{props.titlePart1} <span className='text-blue-600'>{props.titlePart2}</span></h1>
         </div>
-        <div className='w-[80%] min-w-90'>
+        <div className='w-[80%] min-w-90 pl-5 pr-5'>
             <p>{props.description}</p>
         </div>
     </section>

d7316357599ea40f5783bdf4d3c9f4c5b8c2eee0
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-29 13:38:52 -0400

    Se agrego un key necesaria para renderizar de manera ordenada

--- src/app/AboutUs/Data/DataFounder.js
+++ src/app/AboutUs/Data/DataFounder.js
@@ -2,18 +2,21 @@ import ImgPrueba from '@/../public/AboutUs/AboutUsPage/ImgPrueba.png'
 
 const DataFounter = [
     {
+        keyId: 0,
         Img: ImgPrueba,
         name: 'Urpirio Junior Moreno',
         work: 'Software Developer', 
         About: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
     },
     {
+        keyId: 1,
         Img: ImgPrueba,
         name: 'Nelson Javier',
         work: 'Software Developer', 
         About: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus in aut dolore dolor non saepe? Impedit facilis nisi dolore corrupti consectetur aliquam suscipit, ipsam doloremque, molestias, sint odio molestiae!',
     },
     {
+        keyId: 2,
         Img: ImgPrueba,
         name: 'Enmanuel Batuze',
         work: 'Software Developer', 

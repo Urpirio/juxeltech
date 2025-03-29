273418cefe0d29ba71d86ec728e069b5bd79078a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-29 13:49:32 -0400

    Se agrego el identificador de la manera de renderizado

--- src/Data/DataSimpleSolution.js
+++ src/Data/DataSimpleSolution.js
@@ -1,20 +1,24 @@
 const DataSimpleSolution = [
     {
+        keyid: 0,
         number: '1',
         title: 'Contact us',
         description: 'Contact us to boost your brand visibility.',
     },
     {
+        keyid: 1,
         number: '2',
         title: 'Consult',
         description: 'Always available to answer your questions.',
     },
     {
+        keyid: 2,
         number: '3',
         title: 'Place order',
         description: 'Buy our package today to proceed.',
     },
     {
+        keyid: 3,
         number: '4',
         title: 'Payment',
         description: 'We receives payments in all types of banking.',

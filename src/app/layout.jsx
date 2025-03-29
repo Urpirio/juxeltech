e7cc900638824c2277a50cdb93eb5439919a069c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-29 13:40:07 -0400

    SE movio el heade a otra parte

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,26 +1,21 @@
 'use client';
-import React from 'react';
+
 import '../Style/Globals.css';
 import Header from '@/components/Header';
-import logo from '@/../public/SimpleSolution/image.png'
 import Footer from '@/components/Footer';
+import Head from '@/components/Head';
 
 export default function layout({children}) {
 
 
   return (
     <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
-        <title>JuxelTech</title>
-    </head>
-    <body >
+      <Head/>
+    <body>
       <Header/>
         {children}
       <Footer/>
     </body>
     </html>
   )
-}
+};

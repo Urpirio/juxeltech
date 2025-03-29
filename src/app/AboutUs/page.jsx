c916e7b05c0a3dae3bb910c79d94207e36aee379
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-29 13:39:20 -0400

    Se agrego optimizo y solo se tiene que renderizar el componente

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -1,32 +1,12 @@
-import React from 'react';
-import DataAboutSection from './Data/DataAboutSections';
 import AboutSection from './components/AboutSection';
 import Founder from './components/Founder';
 
 export default function About() {
 
-  const ListSection = DataAboutSection.map(DAS => {
-    return(
-      <AboutSection
-      title = {DAS.title}
-      Image = {DAS.Image}
-      Text1 = {DAS.Text1}
-      Text2 = {DAS.Text2}
-      Text3 = {DAS.Text3}
-      classOrder = {DAS.classOrder}
-      classAlign = {DAS.classAlign}
-      padding = {DAS.padding}
-      />
-    )
-  })
-
-
-
   return (
     <main>
-      {ListSection}
+      <AboutSection/>
       <Founder/>
-
     </main>
   )
 };

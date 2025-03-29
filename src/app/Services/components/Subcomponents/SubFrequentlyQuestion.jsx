9fa58d85e96024c6f4c6faf562790519b1cd5fa6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-29 08:16:00 -0400

    Se arreglo la estructura

--- src/app/Services/components/Subcomponents/SubFrequentlyQuestion.jsx
+++ src/app/Services/components/Subcomponents/SubFrequentlyQuestion.jsx
@@ -11,9 +11,8 @@ export default function SubFrequentlyQuestion(props) {
         <div onClick={()=>{DeployQuestion(props.NumberQuestion)}} className='flex items-center justify-between w-[100%] p-2 text-blue-600 border-b-1 border-gray-200 rounded-t-2xl'>
             <h2 className='text-xl '>{props.question}</h2><i className={`bx bx-chevron-right text-4xl transition-transform duration-300 ${props.ClassIcon}`}></i>
         </div>
-        <div className={`hidden text-gray-700 w-[100%] p-1 border-b-1 border-gray-200 
-         `} id={props.classQuetion}>
-            <p className={`${props.Animaciones}`}>
+        <div className={`hidden text-gray-700 p-1 w-[100%] border-b-1 border-gray-200 ${props.Animaciones} `} id={props.classQuetion}>
+            <p>
             {props.answer}
             </p>
         </div>

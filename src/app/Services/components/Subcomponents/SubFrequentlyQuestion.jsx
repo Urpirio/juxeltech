2b1a5463263f9d9f6b321900ce032efa50a90e02
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-29 13:42:18 -0400

    Se optimizo el codigo y se renderiza directamente en el componente

--- src/app/Services/components/Subcomponents/SubFrequentlyQuestion.jsx
+++ src/app/Services/components/Subcomponents/SubFrequentlyQuestion.jsx
@@ -1,13 +1,12 @@
-'use client'
-import React from 'react'
+'use client';
+import DataFrenquentlyQuestion from '../../Data/FrequentlyQuestion/DataFrequentlyQuestion';
 import DeployQuestion from '../../func/DeployQuestion'
 
-export default function SubFrequentlyQuestion(props) {
+export default function SubFrequentlyQuestion() {
 
-
-
-  return (
-    <article className='flex flex-col items-center p-1'>
+  const list = DataFrenquentlyQuestion.map( props => {
+    return(
+      <article key={props.keyId} className='flex flex-col items-center p-1'>
         <div onClick={()=>{DeployQuestion(props.NumberQuestion)}} className='flex items-center justify-between w-[100%] p-2 text-blue-600 border-b-1 border-gray-200 rounded-t-2xl'>
             <h2 className='text-xl '>{props.question}</h2><i className={`bx bx-chevron-right text-4xl transition-transform duration-300 ${props.ClassIcon}`}></i>
         </div>
@@ -17,5 +16,9 @@ export default function SubFrequentlyQuestion(props) {
             </p>
         </div>
     </article>
-  )
-}
+    )
+  })
+
+
+  return (list)
+};

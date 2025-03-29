8878247f9a86408b7488f1076a4db2f4f36223b5
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-29 13:45:24 -0400

    Se creo el componente Head

---
+++ src/components/Head.jsx
@@ -0,0 +1,13 @@
+import Titles from "./SubComponents/SubHead";
+
+export default function Head() {
+
+  return (
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
+        <Titles/>
+    </head>
+  )
+}

6ed22b95d38e21f148ec4a842264bf08b3737cdb
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-29 13:48:19 -0400

    Se restructuro toda la informacion y a se agrego el keyID

--- src/Data/DataBestServices.js
+++ src/Data/DataBestServices.js
@@ -1,27 +1,35 @@
 const DataBestServices = [
     {
+       keyId: 0,
        Service: 'SEO Optimization',
        Description: 'Improved search engine ranking for more visibility.',
        Icon: 'bx bx-search-alt', 
        bg: 'bg-amber-400',
+       link: '',
     },
     {
-        Service: 'UI/UX Design',
-        Description: 'Attractive and optimized design for the best user experience.',
-        Icon: 'bx bx-paint-roll', 
-        bg: 'bg-blue-600',
-     },
-     {
-        Service: 'Web Development',
-        Description: 'Custom and functional website creation.',
-        Icon: 'bx bx-devices',
-        bg: 'bg-violet-600 ',
-     },
-     {
-        Service: 'Maintenance & Support',
-        Description: 'Ongoing updates, security, and performance enhancements.',
-        Icon: 'bx bxs-hard-hat',
-        bg: 'bg-orange-600', 
-     },
+      keyId: 1,
+      Service: 'UI/UX Design',
+      Description: 'Attractive and optimized design for the best user experience.',
+      Icon: 'bx bx-paint-roll', 
+      bg: 'bg-blue-600',
+      link: '',
+    },
+    {
+      keyId: 2,
+      Service: 'Web Development',
+      Description: 'Custom and functional website creation.',
+      Icon: 'bx bx-devices',
+      bg: 'bg-violet-600 ',
+      link: '',
+    },
+    {
+      keyId: 3,
+      Service: 'Maintenance & Support',
+      Description: 'Ongoing updates, security, and performance enhancements.',
+      Icon: 'bx bxs-hard-hat',
+      bg: 'bg-orange-600', 
+      link: '',
+   },
 ];
 export default DataBestServices;

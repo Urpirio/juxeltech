1d6be836ab6f3949456ac5fd79483bdd1959f44c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-30 07:27:01 -0400

    Se reestructuro el componente

--- src/app/Services/components/Subcomponents/SubFrequentlyQuestion.jsx
+++ src/app/Services/components/Subcomponents/SubFrequentlyQuestion.jsx
@@ -7,13 +7,12 @@ export default function SubFrequentlyQuestion() {
   const list = DataFrenquentlyQuestion.map( props => {
     return(
       <article key={props.keyId} className='flex flex-col items-center p-1'>
-        <div onClick={()=>{DeployQuestion(props.NumberQuestion)}} className='flex items-center justify-between w-[100%] p-2 text-blue-600 border-b-1 border-gray-200 rounded-t-2xl'>
-            <h2 className='text-xl '>{props.question}</h2><i className={`bx bx-chevron-right text-4xl transition-transform duration-300 ${props.ClassIcon}`}></i>
+        <div onClick={()=>{DeployQuestion(props.NumberQuestion)}} className='flex items-center justify-between w-[100%] p-2 text-blue-600 border-b-1 border-gray-200 rounded-t-2xl'>  
+            <h2 className='text-xl '>{props.question}</h2>
+            <i className={`bx bx-chevron-right text-4xl transition-transform duration-300 ${props.ClassIcon}`}></i>
         </div>
         <div className={`hidden text-gray-700 p-1 w-[100%] border-b-1 border-gray-200 ${props.Animaciones} `} id={props.classQuetion}>
-            <p>
-            {props.answer}
-            </p>
+            <p>{props.answer}</p>
         </div>
     </article>
     )

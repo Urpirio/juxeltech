2a20d85f844a3cb15023f2b4ec3c9a4afb3a4c89
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-30 07:27:21 -0400

    Se movio a una carpeta diferente

---
+++ src/app/Services/Data/CardServices/DataCardServices.js
@@ -0,0 +1,89 @@
+const DataCardServices = [
+    {
+        keyId: 0,
+        NameService: `Onepage Website`,
+        PriceService: `110 - 500`,
+        DescriptionService: `A single-page website with smooth
+        navigation, ideal for presenting information concisely
+        and effectively.`,
+        classIcon: `bx bx-window`,
+        BgHeader: `bg-amber-400`,
+        BgFooterBtn: `bg-amber-400`,
+        listdescription: [
+            {
+                about: `✔ Minimalist and modern design`,
+            },
+            {
+                about: `✔ Fast loading and improved user experience`,
+            },
+            {
+                about: `✔ Perfect for portfolios, events, and landing pages`,
+            }
+        ]
+    },
+    {
+        keyId: 1,
+        NameService: `Multipage Website`,
+        PriceService: `500 - 1000`,
+        DescriptionService: `A website with multiple interconnected
+        pages, ideal for businesses with more structured content.`,
+        classIcon: `bx bx-windows`,
+        BgHeader: `bg-blue-600`,
+        BgFooterBtn: `bg-blue-600`,
+        listdescription: [
+            {
+                about: `✔ Clear information organization`,
+            },
+            {
+                about: `✔ Improved SEO and user navigation`,
+            },
+            {
+                about: `✔ Great for businesses with multiple services`,
+            }
+        ]
+
+    },
+    {
+        keyId: 2,
+        NameService: `Dynamic Website`,
+        PriceService: `1000 - 2000`,
+        DescriptionService: `A website with interactive content,
+        real-time data management, and advanced functionalities.`,
+        classIcon: `bx bx-buildings`,
+        BgHeader: `bg-violet-700`,
+        BgFooterBtn: `bg-violet-700`,
+        listdescription: [
+            {
+                about: `✔ User and content management`,
+            },
+            {
+                about: `✔ Personalized user interactions`,
+            },
+            {
+                about: `✔ Integration with external systems and databases`,
+            }
+        ]
+    },
+    {
+        keyId: 3,
+        NameService: `E-commerce Website`,
+        PriceService: `1500 - 2500`,
+        DescriptionService: `An online store with payment systems,
+        shopping cart, and product management.`,
+        classIcon: `bx bx-store-alt`,
+        BgHeader: `bg-orange-600`,
+        BgFooterBtn: `bg-orange-600`,
+        listdescription: [
+            {
+                about: `✔ Secure and optimized sales platform`,
+            },
+            {
+                about: `✔ Integration with payment gateways and shipping services`,
+            },
+            {
+                about: `✔ Intuitive design to improve conversions`,
+            }
+        ]
+    },
+];
+export default DataCardServices;

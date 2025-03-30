7e1f3c04cdad8415224268d5b78c89a051273b92
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-30 07:26:47 -0400

    Se reestructuro el componente

--- src/app/Services/components/Subcomponents/SubCardServices.jsx
+++ src/app/Services/components/Subcomponents/SubCardServices.jsx
@@ -1,7 +1,10 @@
+import DataCardServices from "../../Data/CardServices/DataCardServices"
 
-export default function SubCardServices(props) {
-  return (
-    <article className='w-70 flex flex-col  border-1 bg-white border-gray-300 rounded-2xl shadow-xl transition-transform duration-300 hover:scale-105 '>
+export default function SubCardServices() {
+
+    const CardList = DataCardServices.map( props => {
+        return(
+            <article key={props.keyId} className='w-70 flex flex-col  border-1 bg-white border-gray-300 rounded-2xl shadow-xl transition-transform duration-300 hover:scale-105 '>
         <header className='flex items-center rounded-t-2xl'>
             <div  className={`flex justify-center items-center w-[30%] h-16 rounded-tl-2xl rounded-br-2xl p-3   ${props.BgHeader}`}>
                 <i className={`${props.classIcon} text-3xl text-white`}></i>
@@ -20,12 +23,20 @@ export default function SubCardServices(props) {
                 </p>
             </div>
             <div className='flex flex-col text-gray-600'>
-                {props.listdescription}
+                <span>{props.listdescription[0].about}</span>
+                <span>{props.listdescription[1].about}</span>
+                <span>{props.listdescription[2].about}</span>
             </div>
         </section>
         <footer className='h-15 flex justify-center items-center rounded-b-2xl px-2'>
             <button className={`border-1 text-white shadow-2xs rounded-2xl p-2 w-[100%] cursor-pointer transition-opacity duration-200 hover:opacity-80 ${props.BgFooterBtn}`}>Get Service</button>
         </footer>
     </article>
-  )
+        )
+    })
+
+
+
+
+  return (CardList)
 }

7fc4f98dd3ec132d1a11034d6d9603d076c30157
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-01 08:15:58 -0400

    Se agrego la funcion de despliegue

---
+++ src/func/Deploy/DeployMenu.js
@@ -0,0 +1,31 @@
+
+export let IsDeployMenu = [
+    {
+        Boolean: false,
+    }
+];
+
+
+export const DeployMenu = () => {
+    const IconMenu =  document.getElementById('IconMenu');
+    const MovilMenu = document.getElementById('MovilMenu');
+    if(IsDeployMenu[0].Boolean === false){
+        MovilMenu.style.display = 'flex'
+        IconMenu.className = 'bx bx-x';
+        IsDeployMenu[0].Boolean = true;
+    }else{
+        MovilMenu.style.display = 'none';
+        IconMenu.className = 'bx bx-menu';
+        IsDeployMenu[0].Boolean = false;
+    };
+
+};
+//Esto evalua si se esta haciendo scroll  y si el menu esta dezplegado.
+window.onscroll = () =>{
+    if(IsDeployMenu[0].Boolean === true){
+        MovilMenu.style.display = 'none';
+        IconMenu.className = 'bx bx-menu';
+        IsDeployMenu[0].Boolean = false;
+    }else{
+    }
+};

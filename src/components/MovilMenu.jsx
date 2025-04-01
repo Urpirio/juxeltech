2e56bf19eb593f3e1f7e7e2bb382f5f50fc29ac6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-01 11:12:54 -0400

    se agrego el modo oscuro

--- src/components/MovilMenu.jsx
+++ src/components/MovilMenu.jsx
@@ -5,9 +5,9 @@ export default function MovilMenu() {
   
 
   return (
-    <dialog id="MovilMenu" className="hidde">
-        <nav className="w-[100%] border-1 border-gray-400 fixed bg-white  rounded-b-2xl p-5">
-            <ul className="flex justify-around font-semibold">
+    <dialog id="MovilMenu" className="hidde  ">
+        <nav className="w-[100%] fixed border-b-1  border-gray-300 shadow-2xl bg-white dark:bg-black dark:text-white  rounded-b-2xl p-5">
+            <ul className="flex justify-around">
                 <li onClick={()=>{GeneralLink('/')}}>Home</li>
                 <li onClick={()=>{GeneralLink('/AboutUs')}}>About Us</li>
                 <li onClick={()=>{GeneralLink('/Services')}}>Services</li>

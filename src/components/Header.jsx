d659a35f43748053d2f18b4481b24bff39286938
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-01 11:12:50 -0400

    se agrego el modo oscuro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
     
 
   return (
-    <header className='flex items-center justify-around h-15  sticky top-[0%] bg-white dark:bg-black dark:text-white  shadow-xs z-50 p-5'>
+    <header className='flex items-center justify-around h-15  sticky top-[0%] bg-white dark:bg-black dark:border-gray-900 dark:border-b-1 dark:text-white dark:shadow-gray-800 dark:shadow-xs shadow-xs z-50 p-5'>
         <div className='text-2xl flex flex-grow basis-0'>
             <h1>
                 <span className='font-bold text-blue-700'>JX</span>
@@ -66,7 +66,7 @@ export default function Header() {
 
         </nav>
         <div className='flex justify-end items-center flex-grow basis-0'>
-            <button className='hidden sm:flex justify-center items-center border-1 rounded-2xl bg-blue-500 shadow-xs p-2 w-30 text-white font-semibold transition-all duration-200 cursor-pointer hover:opacity-75'>Contact</button>
+            <button className='hidden sm:flex justify-center items-center border-1 rounded-2xl dark:border-black bg-blue-500 shadow-xs p-2 w-30 text-white font-semibold transition-all duration-200 cursor-pointer hover:opacity-75'>Contact</button>
             <button onClick={DeployMenu} className='sm:hidden text-4xl text-blue-900'><i className={'bx bx-menu'} id='IconMenu' ></i></button>
         </div>
     </header>

3798688049719ad013b0f94954e4b495ccfb50d7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-01 11:08:58 -0400

    se agrego el modo oscuro

--- src/components/btns/AboutUsBtn.jsx
+++ src/components/btns/AboutUsBtn.jsx
@@ -2,6 +2,6 @@
 
 export default function AboutUsBtn() {
   return (
-        <button className='border-1 bg-blue-500 text-white font-semibold rounded-2xl p-2 w-30 transition-all duration-200 cursor-pointer hover:opacity-75'>Read more</button>
+        <button className='border-1 dark:border-black bg-blue-500 text-white font-semibold rounded-2xl p-2 w-30 transition-all duration-200 cursor-pointer hover:opacity-75'>Read more</button>
   )
 }

f0ea01db79b4fa78f23659028c24d51f23e261d8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-03 14:33:39 -0400

    Se agrego los componentes del formulario

--- src/app/Services/page.jsx
+++ src/app/Services/page.jsx
@@ -1,11 +1,15 @@
 
 import CardServices from './components/CardServices';
+import Contact from './components/Contact';
+import FormContact from './components/FormContact';
 import FrequentlyQuestions from './components/FrequentlyQuestions';
 
 export default function Services() {
   return (
     <main>
       <CardServices/>
+      <Contact/>
+      <FormContact/>
       <FrequentlyQuestions/>
     </main>
   )

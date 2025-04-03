f1daf5feeac414309be8c75b85454625398928f1
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-03 14:27:47 -0400

    Se agrego un General hover

---
+++ src/app/Services/func/GeneralHovers/GeneralHovers.js
@@ -0,0 +1,8 @@
+
+export const mouseEnter = (id) => {
+    document.getElementById(id).style.opacity = '0.7';
+};
+
+export const mouseLeave = (id) => {
+    document.getElementById(id).style.opacity = '1';
+};

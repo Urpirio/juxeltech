7014dde2196a8fe4031fdd636f5e40bde6c804e3
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-03 14:26:06 -0400

    Se agrego  una section que habla sobre la forma de contacto y acompaña al formulario

---
+++ src/app/Services/components/Contact.jsx
@@ -0,0 +1,18 @@
+
+export default function Contact() {
+  return (
+    <section className="flex flex-col justify-center items-center  gap-2">
+        <div className="flex flex-col items-center gap-1 justify-center text-4xl">
+            <h2>Contact Us</h2>
+            <div className="h-1 w-10 rounded-2xl bg-blue-700"></div>
+        </div>
+        <div className="w-[70%] text-center">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit temporibus
+            odio suscipit quam. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Libero, perspiciatis.
+          </p>
+        </div>
+    </section>
+  )
+}
